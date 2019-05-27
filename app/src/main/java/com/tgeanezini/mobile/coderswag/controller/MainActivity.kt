56e234d852d8c28bf22788c6d6d1db8e5a3d7bea
Tiago Geanezini <tiago.geanezini@inlog.com.br>
2019-05-27 11:13:30 -0300

    feat(category): added list of categories

--- app/src/main/java/com/tgeanezini/mobile/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/tgeanezini/mobile/coderswag/controller/MainActivity.kt
@@ -2,12 +2,21 @@ package com.tgeanezini.mobile.coderswag.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.tgeanezini.mobile.coderswag.R
+import com.tgeanezini.mobile.coderswag.models.Category
+import com.tgeanezini.mobile.coderswag.services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        categoriesListView.adapter = adapter
     }
 }
