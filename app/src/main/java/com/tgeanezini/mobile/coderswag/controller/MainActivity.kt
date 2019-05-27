7c5f236a1d72b9b0ca1bd3226e378b826b660221
Tiago Geanezini <tiago.geanezini@inlog.com.br>
2019-05-27 11:41:07 -0300

    feat(adapter): added custom adapter for categories

--- app/src/main/java/com/tgeanezini/mobile/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/tgeanezini/mobile/coderswag/controller/MainActivity.kt
@@ -4,19 +4,20 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import com.tgeanezini.mobile.coderswag.R
+import com.tgeanezini.mobile.coderswag.adapters.CategoryAdapter
 import com.tgeanezini.mobile.coderswag.models.Category
 import com.tgeanezini.mobile.coderswag.services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var adapter : ArrayAdapter<Category>
+    private lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
         categoriesListView.adapter = adapter
     }
 }
